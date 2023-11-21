f3da54cdff1d810b9da3c4aae0489bd0a0880264
QuentinPerret <quentinprrt33@gmail.com>
2023-11-21 15:15:11 +0100

    :video update

--- PFE_lastrun.py
+++ PFE_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.3),
-    on novembre 10, 2023, at 13:35
+    on novembre 21, 2023, at 15:12
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
